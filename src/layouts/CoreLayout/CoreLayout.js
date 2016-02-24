0a3dc9599ffed192b574e3dcc322a66bf898a160
Tim Man <timothyc.man@gmail.com>
2016-02-24 00:56:02 +0000

    remove all app code specific to the counter example
    and created blank redux modules jsonPane, listingsPane, letters

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,15 +1,6 @@
 import React, { PropTypes } from 'react'
 import '../../styles/core.scss'
 
-// Note: Stateless/function components *will not* hot reload!
-// react-transform *only* works on component classes.
-//
-// Since layouts rarely change, they are a good place to
-// leverage React's new Stateless Functions:
-// https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
-//
-// CoreLayout is a pure function of its props, so we can
-// define it with a plain javascript function...
 function CoreLayout ({ children }) {
   return (
     <div className='page-container'>
@@ -19,7 +10,6 @@ function CoreLayout ({ children }) {
     </div>
   )
 }
-
 CoreLayout.propTypes = {
   children: PropTypes.element
 }
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
-import counter from './modules/counter'
+// import reducers here
 
 export default combineReducers({
-  counter,
+  // reducers go here
   router
 })
