710c6fb06de8da6c34f602f0e445d02356b08c2f
Tim Man <timothyc.man@gmail.com>
2016-02-29 01:12:42 +0000

    refactor to use bootstrap row classes

--- src/components/ListingsCards.js
+++ src/components/ListingsCards.js
@@ -7,11 +7,14 @@ type Props = {
   listingsItems: Array
 };
 
+export const Row = ({children}) =>
+  <div className='row'>{children}</div>
+
 export const Card = ({item: {image, title}}) => {
   return (
-    <div className='col-sm-6 col-md-4'>
+    <div className='col-sm-12 col-md-4 col-ld-3'>
       <div className='thumbnail'>
-        <img src={image} alt={title} />
+        <img className='img-responsive' src={image} alt={title} />
         <div className='caption'>
           <h4>{title}</h4>
         </div>
@@ -29,22 +32,31 @@ export class ListingsCards extends React.Component<void, Props, void> {
     return image.replace('{recipe}', '406x228')
   }
 
-  displayListingsCards () {
+  createListingsCards () {
+    return this.props.listingsItems
+      .map((item) => {
+        item.image = this.pickImageSize(item.image)
+        return <Card item={item} key={item.id}/>
+      })
+  }
+
+  createCardRows () {
     if (!isEmpty(this.props.listingsItems)) {
-      const listings = this.props.listingsItems
-        .map((item) => {
-          item.image = this.pickImageSize(item.image)
-          return <Card item={item} key={item.id}/>
-        })
-      // console.log(listings)
-      return listings
+      const cards = this.createListingsCards()
+      let rows = []
+      let row = cards.splice(0, 3)
+      while (!isEmpty(row)) {
+        rows.push(<Row children={row} key={rows.length}/>)
+        row = cards.splice(0, 3)
+      }
+      return rows
     }
   }
 
   render () {
     return (
       <div>
-        {this.displayListingsCards()}
+        {this.createCardRows()}
       </div>
     )
   }
